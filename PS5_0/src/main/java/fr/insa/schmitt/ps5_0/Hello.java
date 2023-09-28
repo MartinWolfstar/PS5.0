7b24bc9cfba5f7b5448433ba34d6a13beff55c7e
MartinWolfstar <101189318+MartinWolfstar@users.noreply.github.com>
2023-09-28 17:23:12 +0200

    great commit

---
+++ PS5_0/src/main/java/fr/insa/schmitt/ps5_0/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.schmitt.ps5_0;
+
+/**
+ *
+ * @author schmi
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("coucou");
+    }
+}
